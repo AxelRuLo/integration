bc4c441ff25bb032ae3d5ed9747d24e4d07df39d
Axe L. <68178468+AxelRuLo@users.noreply.github.com>
2024-12-09 02:06:05 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+</p>
+
+### ToolsetAI - Backend
+
+1. Clone the repository
+
+2. Install dependencies
+```bash
+npm install
+```
+
+3. Create .env file and fill the env variables with the .env.template_example file
+
+4. Start DB 
+```bash
+docker-compose up -d
+```
+
+5. Run the app in dev mode
+```bash
+npm run start:dev
+```
---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { Logger, ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const logger = new Logger('main');
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  const config = new DocumentBuilder()
+    .setTitle('Template_10_4_8')
+    .setDescription('Template_10_4_8 documentation')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  logger.log(`Application listening on port ${port}`);
+}
+bootstrap();
